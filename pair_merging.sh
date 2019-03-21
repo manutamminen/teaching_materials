77ab34f81ea7708e2166dba233534fedb3c0f391
Manu Tamminen <mavata@gmail.com>
2019-03-21 12:09:27 +0200

    Added a batch pair merging script

---
+++ pair_merging.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -J pair_merging_job
+#SBATCH -o pair_merging_job_out_%j.txt
+#SBATCH -e pair_merging_job_err_%j.txt
+#SBATCH -t 02:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --array=1-6
+#SBATCH -n 6
+
+# move to the directory where the data files are located
+cd /homeappl/home/matammi/teaching_materials
+
+# set input file to be processed
+name=$(sed -n "$SLURM_ARRAY_TASK_ID"p fastq_files.txt)
+forward_read=$(echo $name | awk -F',' '{print $1}')
+reverse_read=$(echo $name | awk -F',' '{print $2}')
+out_name=$(echo $forward_read | awk -F'_' '{print $1"_"$2"_merged.fastq"}')
+
+# run the analysis command
+vsearch --threads 6 --fastq_mergepairs $forward_read --reverse $reverse_read --fastq_minovlen 200 --fastq_maxdiffs 15 --fastqout $out_name --fastq_eeout
